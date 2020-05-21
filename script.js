206aa549149354f379d79c85ad5f08f52ef2e883
TWCurry <thomas.w.curry@accenture.com>
2020-05-21 22:33:52 +0100

    First mvp

--- script.js
+++ script.js
@@ -6,7 +6,18 @@ $( document ).ready(function() {
     var year = currentDate.getFullYear();
     currentDate = year + '-' + month + '-' + day;
 
-    $.get(`https://api.nasa.gov/neo/rest/v1/feed?start_date=2020-05-18&end_date=${currentDate}&api_key=DEMO_KEY`, function(data) {
-        console.log(data);
+    $.get(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${currentDate}&end_date=${currentDate}&api_key=DEMO_KEY`, function(data) {
+        console.log(data)
+        displayData(data, currentDate);
     });
 });
+
+function displayData(data, currentDate){
+    tableHtml = "<table><tr><th>Name</th><th>Diameter</th><th>Hazardous?</th><th>Close Approach Date</th><th>Close Approach Distance</th></tr>";
+    data["near_earth_objects"][currentDate].forEach(element => {
+        tableHtml += `<tr><td>${element["name"]}</td><td>Max: ${element["estimated_diameter"]["meters"]["estimated_diameter_max"]} Min: ${element["estimated_diameter"]["meters"]["estimated_diameter_min"]}</td><td>${element["is_potentially_hazardous_asteroid"]}</td>`;
+        tableHtml += `<td>${element["close_approach_data"][0]["close_approach_date_full"]}</td><td>${element["close_approach_data"][0]["miss_distance"]["kilometers"]}</td></tr>`;
+    });
+    tableHtml += "</table>";
+    $("#main").html(tableHtml);
+}
